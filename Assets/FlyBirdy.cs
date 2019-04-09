e07f7828f32c03cc530f820a23c9c6e7a3b2eeff
robertobradley <rbradley@csumb.edu>
2019-04-08 18:18:40 -0700

    comitting everything
    just comitting my project to github so I can work on it.

---
+++ Assets/FlyBirdy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyBirdy : MonoBehaviour {
+    Rigidbody rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        print("There was a trigger");
+    }
+}

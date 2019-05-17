87a2cc38c7cb5e49ae03c221f66b370927f62cb6
robertobradley <33430715+robertobradley@users.noreply.github.com>
2019-05-17 01:45:20 -0700

    Cave Detailing
    Added more details to cave and am playing around with water things.

---
+++ Assets/waterScreenChange.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waterScreenChange : MonoBehaviour {
+
+   
+    
+   // private bool inwater = false;
+    //private bool outwater = true;
+  
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player" )
+        {
+           // inwater = true;
+           // outwater = false;
+            Debug.Log("enter");
+            RenderSettings.fog = true;
+
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" )
+        {
+           // inwater = false;
+           // outwater = true;
+            Debug.Log("exits");
+            RenderSettings.fog = false;
+
+        }
+    }
+}
